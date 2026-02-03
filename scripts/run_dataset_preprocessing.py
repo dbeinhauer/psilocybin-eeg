d959bc6e663437af8883ed00ae8e249bd21de5a0
David Beinhauer <david.beinhauer@email.cz>
2026-02-03 15:25:51 +0100

    Updating Umbriel script run:

--- scripts/run_dataset_preprocessing.py
+++ scripts/run_dataset_preprocessing.py
@@ -3,6 +3,12 @@ Script to run all dataset preprocessing pipeline. Typically used on the
 computational cluster Umbriel.
 """
 
+import sys
+from pathlib import Path
+
+# Add parent directory to path
+sys.path.insert(0, str(Path(__file__).parent.parent))
+
 from src.data.dataset_handler import DatasetHandler
 from src.definitions.fields import ExperimentNames, CoordinateSystems
 
